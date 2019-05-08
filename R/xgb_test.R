9ae3a10a87696686035154cc6c3254fc6ba06c5f
Brent Schneeman <schneeman@gmail.com>
2019-05-08 12:00:12 -0500

    added AUC curve

--- R/xgb_test.R
+++ R/xgb_test.R
@@ -223,3 +223,14 @@ ggplot(tib, aes(x=frac, y=acc, color=exp_name)) +
     y = "Overall Inference Accuracy (OVA)"
   )
 
+ggplot(tib, aes(x=frac, y=auc, color=exp_name)) +
+  geom_line(size=1) + geom_point(color="white", size = 0.2) +
+  scale_color_viridis_d("Model Type") +
+  ggthemes::theme_few() +
+  labs(
+    title = "Model Architectures and Training Batch Size",
+    subtitle = "MNIST Dataset",
+    x = "Fraction of MNIST Training\n(60,000 * x = # of samples)",
+    y = "Overall AUC (OVA)"
+  )
+
