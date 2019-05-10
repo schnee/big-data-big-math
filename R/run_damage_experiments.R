41d20cad035e1c29d75787a9e22665913498771b
Brent Schneeman <schneeman@gmail.com>
2019-05-10 08:36:14 -0500

    broke size and damage into two different exps

---
+++ R/run_damage_experiments.R
@@ -0,0 +1,37 @@
+
+library(keras)
+library(ggplot2)
+library(purrr)
+library(readr)
+library(tibble)
+library(dplyr)
+library(magrittr)
+
+devtools::load_all(here::here("packages/testbench"))
+
+mnist <- keras::dataset_fashion_mnist()
+x_train <- mnist$train$x
+y_train <- mnist$train$y
+x_test <- mnist$test$x
+y_test <- mnist$test$y
+
+damage_tib <- c(0.01) %>%
+  #damage_tib <- c(0:10 / 100, 0.2, 0.3) %>%
+  sort() %>%
+  map_dfr(run_damage_exp, x_train, y_train, x_test, y_test)
+
+damage_tib %>% write_csv("fashion-mnist-damage-results.csv")
+
+damage_tib <- read_csv("fashion-mnist-damage-results.csv")
+
+ggplot(damage_tib, aes(x=frac, y=acc, color=exp_name)) +
+  geom_line(size=1) + geom_point(color="white", size = 0.2) +
+  scale_color_viridis_d("Model Type") +
+  ggthemes::theme_few() +
+  labs(
+    title = "Model Architectures and Training Batch Size",
+    subtitle = "Fashion MNIST Dataset",
+    x = "Fraction of Training\n(60,000 * x = # of samples)",
+    y = "AUC (OVA)"
+  )
+
---
+++ R/run_size_experiments.R
@@ -0,0 +1,49 @@
+
+library(keras)
+library(ggplot2)
+library(purrr)
+library(readr)
+library(tibble)
+library(dplyr)
+library(magrittr)
+
+devtools::load_all(here::here("packages/testbench"))
+
+
+mnist <- keras::dataset_fashion_mnist()
+x_train <- mnist$train$x
+y_train <- mnist$train$y
+x_test <- mnist$test$x
+y_test <- mnist$test$y
+
+#
+tib <- c(0.05) %>%
+tib <- c(1:9 / 100, 1:9 / 10, 91:100 / 100) %>%
+  sort() %>%
+  map_dfr(run_size_exp, x_train, y_train, x_test, y_test)
+
+ tib %>% write_csv("./fashion-data-size-results.csv")
+
+tib <- read_csv("./fashion-data-size-results.csv")
+
+ggplot(tib, aes(x=frac, y=acc, color=exp_name)) +
+  geom_line(size=1) + geom_point(color="white", size = 0.2) +
+  scale_color_viridis_d("Model Type") +
+  ggthemes::theme_few() +
+  labs(
+    title = "Model Architectures and Training Batch Size",
+    subtitle = "MNIST Dataset",
+    x = "Fraction of MNIST Training\n(60,000 * x = # of samples)",
+    y = "Inference Accuracy (OVA)"
+  )
+
+ggplot(tib, aes(x=frac, y=auc, color=exp_name)) +
+   geom_line(size=1) + geom_point(color="white", size = 0.2) +
+   scale_color_viridis_d("Model Type") +
+   ggthemes::theme_few() +
+   labs(
+     title = "Model Architectures and Training Batch Size",
+     subtitle = "MNIST Dataset",
+     x = "Fraction of MNIST Training\n(60,000 * x = # of samples)",
+     y = "AUC (OVA)"
+   )
