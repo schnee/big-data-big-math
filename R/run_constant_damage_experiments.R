ae7c2c0e14194187441cc5ef3b389f3b72345db8
Brent Schneeman <schneeman@gmail.com>
2019-05-13 14:25:04 -0500

    const damage

---
+++ R/run_constant_damage_experiments.R
@@ -0,0 +1,49 @@
+
+library(keras)
+library(ggplot2)
+library(purrr)
+library(readr)
+library(tibble)
+library(dplyr)
+library(magrittr)
+
+devtools::load_all(here::here("packages/testbench"))
+
+mnist <- keras::dataset_fashion_mnist()
+x_train <- mnist$train$x
+y_train <- mnist$train$y
+x_test <- mnist$test$x
+y_test <- mnist$test$y
+
+damage_tib <- c(0.01) %>%
+#damage_tib <- c(0:9 / 100, 1:5 / 10) %>%
+  sort() %>%
+  map_dfr(run_constant_damage_exp, x_train, y_train, x_test, y_test)
+
+damage_tib %>% write_csv("fashion-mnist-const-damage-results.csv")
+
+damage_tib <- read_csv("fashion-mnist-const-damage-results.csv")
+
+ggplot(damage_tib, aes(x=frac, y=acc, color=exp_name)) +
+  geom_line(size=1) + geom_point(color="white", size = 0.2) +
+  ggthemes::scale_color_few("Model Type", palette = "Dark") +
+  ggthemes::theme_few() +
+  scale_x_continuous(labels = scales::percent) +
+  labs(
+    title = "Model Architectures and Mis-labeled Data",
+    subtitle = "Fashion MNIST Dataset",
+    x = "Mis-labeled training data\n(percent of 60,000 obs)",
+    y = "Accuracy (OVA)"
+  )
+
+ggplot(damage_tib, aes(x=frac, y=auc, color=exp_name)) +
+  geom_line(size=1) + geom_point(color="white", size = 0.2) +
+  ggthemes::scale_color_few("Model Type", palette = "Dark") +
+  ggthemes::theme_few() +
+  scale_x_continuous(labels = scales::percent) +
+  labs(
+    title = "Model Architectures and Mis-labeled Data",
+    subtitle = "Fashion MNIST Dataset",
+    x = "Mis-labeled training data\n(percent of 60,000 obs)",
+    y = "AUC (OVA)"
+  )
